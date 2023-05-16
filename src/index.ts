bbe7fe81f0977f164bb5b4ad95243426ddb266c9
Grzegorz Maniak <git@grzegorz.ie>
2023-05-16 23:59:16 +0100

    Laying out basic structs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 
-import init from './wasm/mandelbrot';
+import init, {
+    Graph,
+    GraphInitiator,
+    LineGraph,
+    Point
+} from './wasm/mandelbrot';
 init().then(() => main());
 
 function main() {
     console.info('WASM Bindings loaded');
+
+    // -- Get 
 }
